a0b2653b1fc80c98cf65c702c2db996403480ddc
mammix2 <mammix2@hotmail.com>
2015-03-10 14:15:08 +0000

    Ascension master v1.0.0.6

---
+++ src/genesis.h
@@ -0,0 +1,15 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("000005d98bd5b7308b8650d8052d075fcf3dabba6b64c3f5877580112ab4b83d");
+static const uint256 hashGenesisBlockTestNet("000005d98bd5b7308b8650d8052d075fcf3dabba6b64c3f5877580112ab4b83d");
+static const uint256 fCheckPoint1("0000000e1eb8a8285e8aa83e943a452840d482a03eccb4d653f0a79dca96065f"); // new chain checkpoint at block 70
+
+// TODO: Move Kernel checksum in here
+
+
+#endif // GENESIS_H
